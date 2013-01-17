34427b1485e3e0d54b40f60f8000138ba2bc6106
Jianing Yang <jianingy.yang@gmail.com>
2013-01-17 17:33:09 +0800

    added missing files

---
+++ almar/util.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# filename   : util.py
+# created at : 2013-01-15 16:44:48
+# author     : Jianing Yang <jianingy.yang AT gmail DOT com>
+
+__author__ = 'Jianing Yang <jianingy.yang AT gmail DOT com>'
+
+def quote(s):
+    return s.replace("'", "\\'").replace('"', '\\"')
