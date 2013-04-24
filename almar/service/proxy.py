25735f16b99e88c438d66c78f54144f0e431d9e0
Jianing Yang <jianingy.yang@gmail.com>
2013-04-24 15:07:21 +0800

    fix hash function error

--- almar/service/proxy.py
+++ almar/service/proxy.py
@@ -22,7 +22,7 @@ class AlmarProxyService(jsonrpc.JSONRPC):
 
     def path_hash(self, s):
         # sdbm's hash function
-        reduce(lambda hash_, c: (hash_ << 6) + (hash_ << 16) - hash_ + ord(c), s)
+        reduce(lambda hash_, c: (hash_ << 6) + (hash_ << 16) - hash_ + ord(c), s, 0) % 128
 
     def find_searcher_by_path(self, path):
         # XXX: find a good hash function
