abfa6245923db0c4ab4c44261169fa0cdf966b1c
Jianing Yang <jianingy.yang@gmail.com>
2013-04-25 11:32:25 +0800

    fix == operator for path, model search

--- almar/backend/postgresql.py
+++ almar/backend/postgresql.py
@@ -513,10 +513,10 @@ class PostgreSQLBackend(object):
                     lhs = lhs.strip('_')
                     if term == '==':
                         where_clause = \
-                            "\"%s\" = lower(E'%s')" % (lhs, rhs)
+                            "lower(\"%s\"::text) = lower(E'%s')" % (lhs, rhs)
                     elif term == '!=':
                         where_clause = \
-                            "\"%s\" != lower(E'%s')" % (lhs, rhs)
+                            "lower(\"%s\"::text) != lower(E'%s')" % (lhs, rhs)
                     elif term == '===':
                         where_clause = "\"%s\" = E'%s'" % (lhs, rhs)
                     elif term == '!==':
