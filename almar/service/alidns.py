6247a4ad2a9ede10c0073951fd156efa0daf2ae3
Jianing Yang <jianingy.yang@gmail.com>
2013-05-10 15:44:49 +0800

    add alidns

---
+++ almar/service/alidns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# filename   : alidns.py
+# created at : 2013-05-10 15:44:10
+# author     : Jianing Yang <jianingy.yang AT gmail DOT com>
+
+__author__ = 'Jianing Yang <jianingy.yang AT gmail DOT com>'
